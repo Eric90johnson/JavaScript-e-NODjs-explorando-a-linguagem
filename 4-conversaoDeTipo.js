7d8861ecc0d8df27cba0e91a805733b225d36739
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-02 21:34:14 -0300

    Create 4-conversaoDeTipo.js
    Entendo o funcionamento das conversões de tipos

---
+++ 4-conversaoDeTipo.js
@@ -0,0 +1,25 @@
+console.log();
+console.log("Conversão de tipos");
+
+
+console.log();
+//concatenação
+console.log("ano " + 2022);
+
+console.log();
+//dessa forma junta (concatena) apenas os numeros dando o resultado = 22
+console.log("2" + "2");
+
+console.log();
+//dessa forma soma os dois numeros dando o resultado = 4
+console.log(parseInt("2") + parseInt("2"));
+
+console.log();
+//Nesse tipo de caso o JS converte automaticamente o texto para numero, pois divisão só é possível com numeros dando o resultado = 5
+console.log("10" / "2");
+
+console.log();
+// Nesse caso o interpretador do JS não consegue identificar o tipo de operação que esta sendo solicitado resultado = NaM (Not a Number)
+console.log("Ricardo" / "2");
+
+console.log();
