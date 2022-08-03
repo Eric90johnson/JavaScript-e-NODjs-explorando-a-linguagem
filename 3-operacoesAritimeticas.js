197d7b9cb31617f8127a3b82b99b3f96ce752d99
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-02 21:20:11 -0300

    Update 3-operacoesAritimeticas.js

--- 3-operacoesAritimeticas.js
+++ 3-operacoesAritimeticas.js
@@ -1,3 +1,22 @@
+console.log();
 console.log("Operações Aritiméticas");
 
-console.log(10 + 8 * 2)
+console.log();
+//forma errada
+console.log("Forma Errada!!!");
+console.log(10 + 8 * 2);
+
+console.log();
+//forma errada
+console.log("Forma Correta!!!");
+console.log((10 + 8) * 2);
+
+console.log();
+//concatenação
+console.log("ano " + 2022);
+
+console.log();
+
+
+
+
