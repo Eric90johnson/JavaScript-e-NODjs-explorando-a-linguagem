40fb6651dc56ab3217c7813b23466662423b94c0
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-02 21:09:17 -0300

    Update 2-trabalhandoComVariaveis.js

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com variáveis");
+//console tem que ser escrito sempre com letras minúsculas
+// JS é case sensitive (maiusculas e menusculas devem ser bem definidas)
+
+// boas práticas
+const idade = 29
+const nome = "Eric"
+let ano = 2020
+
+//não fazer
+// mes = janeiro
