e199538638ee34bf1fd7cdacc3945fc282f5f760
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-04 20:40:27 -0300

    Update 5-atribuicaoVariaveis.js

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -2,10 +2,16 @@ console.log();
 console.log("Trabalhando com atribuições de variáveis");
 
 const idade = 29;
-const nome = "Ricardo"
-const sobrenome = "Bugan"
+const nome = "Eric";
+const sobrenome = "Johnson";
 
 console.log();
+//para separar os textos(strings), pode ser feito dessas duas formas, caso contrario as palavras ficarão unidas.
+//console.log(nome, + sobrenome);
 console.log(nome + " " + sobrenome);
 
-console.log();
+//nessa caso para concatenar texto com variáveis usa-se a sentença ${}
+//console.log(`Meu nome é ${nome } ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+
