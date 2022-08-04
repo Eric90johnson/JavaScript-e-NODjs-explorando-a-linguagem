88680be1a4d6021163c311461a7fb8cf4fea2549
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-04 20:19:52 -0300

    Create 5-atribuicaoVariaveis.js
    Aplicando atribuições de variáveis

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,11 @@
+console.log();
+console.log("Trabalhando com atribuições de variáveis");
+
+const idade = 29;
+const nome = "Ricardo"
+const sobrenome = "Bugan"
+
+console.log();
+console.log(nome + " " + sobrenome);
+
+console.log();
