61f15576d3519415337897c600b4614da3c4920f
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-08 22:01:37 -0300

    Create 7-condicionas.js

---
+++ 7-condicionas.js
@@ -0,0 +1,35 @@
+console.log(`Trabalhando com condicionas`)
+
+
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 15;
+const estaAcompanhado = false;
+
+//listaDeDestinos.push(`Curitiba`) 
+
+console.log("Destinos possíveis: ");
+console.log();
+console.log(listaDeDestinos);
+
+if(idadeComprador >= 18) {
+    console.log("Comprador maior de Idade");
+    listaDeDestinos.splice(1,1) //removendo itens
+    
+} else{
+    //a pessoa é menor de idade
+    if(estaAcompanhado) {
+        console.log("Comprador é menor de idade e esta acompanhado");
+        listaDeDestinos.splice(1,1); //
+    } else {
+        console.log("NÃO POSSO VENDER A PASSAGEM: Não é Maior de Idade e não esta acompanhada!");
+    }    
+}
+
+console.log(listaDeDestinos);
+
+console.log();
